68b9525b75e4f08a1d476ebf7aabe485921ac822
Luke <luke@MacBook-Air.local>
2020-02-28 14:26:01 -0600

    now maintains list of correct and incorrect guesses

--- guess.c
+++ guess.c
@@ -28,6 +28,10 @@ void print_past(char* past){
             printf("%c\n", past[i]);
         }
     }
+    if(length == 0){
+       printf("\n"); 
+    }
+    
 }
 // prints hidden word in easy to view format
 void print_hidden(char* hidden){
@@ -36,25 +40,49 @@ void print_hidden(char* hidden){
     printf("%s\n",hidden); 
     printf("----------------\n");
 }
+int repeat_guess(char* letter, char* past, char* past_right){
+    int length_wrong = strlen(past);
+    int length_right = strlen(past_right);
+    int repeat = 0; // false until repeat is discovered
+    for(int i = 0; i < length_wrong; i++){
+        if(past[i] == *letter){
+            repeat = 1;
+        }
+    }
+    for(int i = 0; i < length_right; i++){
+        if(past_right[i] == *letter){
+            repeat = 1;
+        }
+    }
+    return repeat;
+}
 // handles user letter guess, 
 //updates num_correct, num_wrong and hidden word
-int guess(char* hidden, char* word, char* letter, int *length, int *num_correct, int *num_wrong, int *max_wrong, char* past){
+int guess(char* hidden, char* word, char* letter, int *length, 
+            int *num_correct, int *num_wrong, int *max_wrong, char* past, char* past_right){
     int match = 0; // false unless guess is a match
     for(int i = 0; i < *length; i++){
+        if(repeat_guess(letter, past, past_right)){
+            printf("\nYou already guessed %c", *letter);
+            match = -1;
+            break; // stop searching for matches
+        }
         if(word[i] == *letter){
             match = 1;
             hidden[i] = *letter;
             *num_correct += 1; // increment correct count
         }
     }
-    if(!match){
+    if(match == 0){
         past[*num_wrong] = *letter; // add letter to list of past wrong guesses
         past[(*num_wrong)+1] = '\0'; // mark end 
         *num_wrong += 1; // increment wrong count
         printf("Wrong!");
         
     }
-    else{
+    else if(match == 1){
+        past_right[*num_wrong] = *letter; // add letter to list of past right
+        past_right[(*num_wrong)+1] = '\0'; // mark end
         printf("Correct!");
     }
     printf(" - %d incorrect guesses left\n", *max_wrong-*num_wrong);
@@ -135,13 +163,15 @@ int main(int argc, char *argv[])
     int num_wrong = 0; // total wrong guesses
     char past[64]; // holds past incorrect guesses
     past[0] = '\0'; // avoid unexpected array values 
+    char past_right[64]; // holds past correct guesses
+    past_right[0] = '\0'; // avoid unecpected values
     char letter = '\0'; // make room for user char guess
     // repeatedly prompt user for guess input
     while(( max_wrong > num_wrong ) && ( num_correct < length ) ){
         printf("Enter letter guess:  ");
         letter = '\0';
         scanf("%c", &letter); // reads user input
-        guess(hidden, word, &letter, &length, &num_correct, &num_wrong, &max_wrong, past);
+        guess(hidden, word, &letter, &length, &num_correct, &num_wrong, &max_wrong, past, past_right);
         while ( getchar() != '\n' ); // clear ENTER key from buffer
     }
 
